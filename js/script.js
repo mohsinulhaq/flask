1c8098f5d2a560782193f50c8a2d54a45e1df6a2
Mohsin Ul Haq <mohsinulhaq01@gmail.com>
2015-12-11 13:25:50 +0530

    show more data, display in tabular format

--- js/script.js
+++ js/script.js
@@ -1,17 +1,14 @@
 $(function() {
 	$('.search-button').click(function() {
-		var searchBox = $('.search-box');
-		$.ajax({
-			url: 'https://api.github.com/search/users',
-			type: 'GET',
-			dataType: 'json',
-			data: {'q': searchBox.val().trim()},
-			success: function(data) {
-				$('.image').attr({src:data.items[0].avatar_url, height:'250px',width:'250px'});
-				$('.profile').attr('href', data.items[0].html_url).text('Profile link');
-				$('.id').text('Github ID: ' + data.items[0].id);
-				$('.github-score').text('Github Score: ' + data.items[0].score);
-			}
-		});
+		var searchBox = $('.search-box').val().trim();
+		$.getJSON('https://api.github.com/users/' + searchBox, function(data) {
+				$('.image').attr({src:data.avatar_url, height:'150px',width:'150px'});
+				$('.name').text(data.name);
+				$('.profile').attr('href', data.html_url).text('Click here');
+				$('.id').text(data.id);
+				$('.followers').text(data.followers);
+				$('.repos').text(data.public_repos);
+				$('.table').show();
+			});
 	});
 });
